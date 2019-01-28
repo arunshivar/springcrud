ed5afb47cd8588260b3926bbb4cf18d84c890704
arun.rapelli <arun.rapelli@india.semanticbits.com>
2019-01-28 20:01:50 +0530

    First commit

---
+++ src/test/java/com/arun/SprincrudApplicationTests.java
@@ -0,0 +1,17 @@
+package com.arun;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class SprincrudApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
